046655efc11626656c4f0b63b6445ddd2ce9edb5
bdcabreran <bayron.cabrera@titoma.com>
2023-02-17 17:28:50 -0500

    Docs ( Adding RTT project )

---
+++ Nucleo-F103RB/01_rtt_logger/Debug/APIs/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (10.3-2021.10)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../APIs/itm.c \
+../APIs/log.c 
+
+OBJS += \
+./APIs/itm.o \
+./APIs/log.o 
+
+C_DEPS += \
+./APIs/itm.d \
+./APIs/log.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+APIs/%.o APIs/%.su: ../APIs/%.c APIs/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -I../Middlewares/Third_Party/FreeRTOS/Source/include -I../Middlewares/Third_Party/FreeRTOS/Source/CMSIS_RTOS_V2 -I../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM3 -I../APIs -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-APIs
+
+clean-APIs:
+	-$(RM) ./APIs/itm.d ./APIs/itm.o ./APIs/itm.su ./APIs/log.d ./APIs/log.o ./APIs/log.su
+
+.PHONY: clean-APIs
+
